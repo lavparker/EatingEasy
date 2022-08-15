b046dcd3d07efbc61a9b9fdf83d6c473c4bb9ed8
lavparker <lavinia.parker@outlook.com>
2022-08-15 11:40:14 -0400

    added json view files

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api defaults: {format: :json} do
+    resources :users, only: [:create]
+    resource :session, only: [:create, :destroy]
+    # resources :restaurants, only: []
+    # resources :reservations, only: []
+    # resources :reviews, only: []
+    # resources :favorites, only: []
+  end
 end
