426d424cd49806924b2bd65f5f2bf2f4a456e76f
lavparker <lavinia.parker@outlook.com>
2022-08-15 09:56:16 -0400

    added controller files

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+    validates :first_name, :last_name, :username, :email, :password_digest, presence: true
+    validates :username, :email, uniqueness: true
+    validates :password length: {minimum: 6, allow_nil: true} 
+
+    has_many :reservations
+    has_many :reviews
+    has_many :favorites
+    
+    
+end
