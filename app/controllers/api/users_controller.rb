dff9afb4db31ba7a14250952ef5572a080711753
lavparker <lavinia.parker@outlook.com>
2022-08-15 11:21:59 -0400

    added controllers

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,2 +1,18 @@
 class Api::UsersController < ApplicationController
+     def create
+    @user = User.new(user_params)
+    if @user.save
+      login!(@user)
+      redirect_to users_url
+    else
+      flash.now[:errors] = @user.errors.full_messages
+      render :new
+    end
+  end
+
+  private
+    def user_params
+        params.require(:user).permit(:first_name, :last_name, :email, :username, :password)
+    end
+
 end
