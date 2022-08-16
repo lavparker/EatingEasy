5a89c384383477cd08360315054f7113503f9153
lavparker <lavinia.parker@outlook.com>
2022-08-16 11:22:49 -0400

    completed user.rb model

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,27 +1,27 @@
 class Api::SessionsController < ApplicationController
-  # before_action :require_logged_in, only: [:destroy]
+  before_action :require_logged_in, only: [:destroy]
 
-  # def create
-  #   # @user = User.find_by_credentials(
-  #   #   params[:user][:username],
-  #   #   params[:user][:password]
-  #   # )
+  def create
+    @user = User.find_by_credentials(
+      params[:user][:username],
+      params[:user][:password]
+    )
 
-  #   # if @user
-  #   #   login!(@user)
-  #   #   redirect_to users_url
-  #   # else
-  #   #   flash.now[:errors] = ['Invalid username or password'], status: 401
-  #   #   render :new
-  #   # end
-  # end
+    if @user
+      login!(@user)
+      redirect_to users_url
+    else
+      flash.now[:errors] = ['Invalid username or password'], status: 401
+      render :new
+    end
+  end
 
-  # def destroy
+  def destroy
     
-  #   logout!
-  #   render json: {}
+    logout!
+    render json: {}
 
-  #   # render a Render a 404 message if there is no current_user to logout.
+    # render a Render a 404 message if there is no current_user to logout.
 
-  # end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,51 @@
 class User < ApplicationRecord
     validates :first_name, :last_name, :username, :email, :password_digest, presence: true
     validates :username, :email, uniqueness: true
-    # validates :password, length: {minimum: 6, allow_nil: true} 
+    validates :password, length: {minimum: 6, allow_nil: true} 
+    validates :session_token, presence: true, uniqueness: true 
+    attr_reader: :password
 
-    has_many :reservations
-    has_many :reviews
-    has_many :favorites
+    has_many :reservations,
+        foreign_key: :user_id,
+        class_name: "Reservation"
+
+    has_many :reviews,
+        foreign_key: :user_id,
+        class_name: "Review"
+
+    has_many :favorites,
+        foreign_key: :user_id,
+        class_name: "Favorite"
+
+    after_initialize :ensure_session_token
+
+    def self.find_by_credentials(username, password)
+        user = User.find_by(username: username)
+
+        return nil if user.nil?
+        user.is_password?(password) ? user : nil 
+    end
+
+    def is_password?(password)
+        BCrypt::password.new(self.password_digest).is_password?(password)
+    end
+
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def reset_session_token!
+        self.session_token = SecureRandom.base64(64)
+        self.save!
+        self.session_token
+    end
+
+    private
+    def ensure_session_token
+        self.session_token ||= SecureRandom.base64(64)
+    end
     
+
     
 end
