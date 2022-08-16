461749551ab6705cab17ba0dee8fd8512123bf7c
lavparker <lavinia.parker@outlook.com>
2022-08-16 10:43:20 -0400

    commented out unused code in controllers

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
     validates :first_name, :last_name, :username, :email, :password_digest, presence: true
     validates :username, :email, uniqueness: true
-    validates :password, length: {minimum: 6, allow_nil: true} 
+    # validates :password, length: {minimum: 6, allow_nil: true} 
 
     has_many :reservations
     has_many :reviews
