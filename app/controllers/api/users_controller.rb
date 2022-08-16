811fa128e2237fb167dfefe1d0f62e8513221087
lavparker <lavinia.parker@outlook.com>
2022-08-16 14:11:53 -0400

    hashed out changes in routes

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,6 +1,6 @@
 class Api::UsersController < ApplicationController
     skip_before_action :verify_authenticity_token, only: [:create]
-    before_action :require_logged_in, only: [:index, :show]
+    # before_action :require_logged_in, only: [:index, :show]
      def create
     @user = User.new(user_params)
     if @user.save
--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
     resources :users, only: [:create]
     resource :session, only: [:create, :destroy]
-    # resources :restaurants, only: [:index, :show]
-    # resources :reservations, only: [:show, :index, :create, :destroy]
-    # resources :reviews, only: [:create, :destroy, :index, :show, :update]
-    # resources :favorites, only: [:create, :destroy, :index, :show]
+    
   
   end
 
