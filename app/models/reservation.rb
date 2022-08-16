dfe6e8ce28ef5d869122b9604b69fe98ea5da937
lavparker <lavinia.parker@outlook.com>
2022-08-16 10:29:15 -0400

    fixed but in user models

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,7 +1,7 @@
-class Reservation < ApplicationRecord
-    validates :restaurant_id, :first_name, :last_name, :phone_number, :email, :date, :time, :party_size, :special_requests, presence: true
-    validates :restaurant_id, uniqueness: true 
+# class Reservation < ApplicationRecord
+#     validates :restaurant_id, :first_name, :last_name, :phone_number, :email, :date, :time, :party_size, :special_requests, presence: true
+#     validates :restaurant_id, uniqueness: true 
     
-    belongs_to :user, optional: true 
-    belongs_to :restaurant
-end
+#     belongs_to :user, optional: true 
+#     belongs_to :restaurant
+# end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,11 +1,11 @@
-class Restaurant < ApplicationRecord
-    validates :name, :address, :phone_number, :cuisine, :details, :hours, :dress_code, :website, :dining_style, presence: true 
-    validates :address, :phone_number, uniqueness: true
+# class Restaurant < ApplicationRecord
+#     validates :name, :address, :phone_number, :cuisine, :details, :hours, :dress_code, :website, :dining_style, presence: true 
+#     validates :address, :phone_number, uniqueness: true
 
-    has_many :reservations
-    has_many :reviews 
-    has_many :favorites  
+#     has_many :reservations
+#     has_many :reviews 
+#     has_many :favorites  
 
     
-end
+# end
 
