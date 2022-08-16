bd80375abe6c6f5547b4f78efb944763f5ebfadf
lavparker <lavinia.parker@outlook.com>
2022-08-16 10:21:04 -0400

    pushing to heroku

--- frontend/reducers/session_reducer.js
+++ frontend/reducers/session_reducer.js
@@ -1,20 +1,20 @@
-import { LOGOUT_CURRENT_USER, RECEIVE_CURRENT_USER } from "../actions/session_actions";
+// import { LOGOUT_CURRENT_USER, RECEIVE_CURRENT_USER } from "../actions/session_actions";
 
-const ... = (oldState = {}, action) =>{
-    Object.freeze(oldState)
+// const sessionReducer = (oldState = {}, action) =>{
+//     Object.freeze(oldState)
 
-    switch (action.type) {
-        case RECEIVE_CURRENT_USER:
-            return Object.assign({}, oldState, action.)
-        case LOGOUT_CURRENT_USER:
-            return Object.assign({}, oldState, action.)
+//     switch (action.type) {
+//         case RECEIVE_CURRENT_USER:
+//             return Object.assign({}, oldState, action.)
+//         case LOGOUT_CURRENT_USER:
+//             return Object.assign({}, oldState, action.)
             
       
     
-        default:
-            return oldState
+//         default:
+//             return oldState
 
-    }
-}
+//     }
+// }
 
-export default sessionReducer
+// export default sessionReducer; 
