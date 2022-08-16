5e6092ef758dea32573c5249e75133b18a5e79de
lavparker <lavinia.parker@outlook.com>
2022-08-16 15:44:01 -0400

    created rootReducer file

--- frontend/reducers/session_reducer.js
+++ frontend/reducers/session_reducer.js
@@ -1,20 +1,22 @@
-// import { LOGOUT_CURRENT_USER, RECEIVE_CURRENT_USER } from "../actions/session_actions";
+import { LOGOUT_CURRENT_USER, RECEIVE_CURRENT_USER } from "../actions/session_actions";
 
-// const sessionReducer = (oldState = {}, action) =>{
-//     Object.freeze(oldState)
+const _nullSession = {
+    currentUser: null,
+};
 
-//     switch (action.type) {
-//         case RECEIVE_CURRENT_USER:
-//             return Object.assign({}, oldState, action.)
-//         case LOGOUT_CURRENT_USER:
-//             return Object.assign({}, oldState, action.)
-            
-      
+const sessionReducer = (oldState = _nullSession, action) =>{
+    Object.freeze(oldState)
+
+    switch (action.type) {
+        case RECEIVE_CURRENT_USER:
+            return Object.assign({}, { currentUser: action.user});
+        case LOGOUT_CURRENT_USER:
+           return _nullSession; 
     
-//         default:
-//             return oldState
+        default:
+            return oldState
 
-//     }
-// }
+    }
+};
 
-// export default sessionReducer; 
+export default sessionReducer; 
