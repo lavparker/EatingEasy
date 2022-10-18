10a30411f0311244e20e215f03ebd2615b020880
lavparker <lavinia.parker@outlook.com>
2022-10-18 09:56:54 -0400

    css to show page

--- frontend/components/search/search_form.jsx
+++ frontend/components/search/search_form.jsx
@@ -1,5 +1,5 @@
-// import React from 'react';
-// import { FaSearch } from 'react-icons/fa'
+import React from 'react';
+import { FaSearch } from 'react-icons/fa'
 
 // class SearchForm extends React.Component{
 //     constructor(props){
@@ -12,58 +12,55 @@
 //         this.handleSubmit = this.handleSubmit.bind(this); 
 //     }
 
-//     handleInput = resName => {
-//         return e => { this.setState({
-//             resName: e.target.value
-//         })}
-
-        
-
-//     }
-
-
-// }
-
-// class SearchForm extends React.Component{
-//     constructor(props){
-
-//     }
-
-//     constructor(props){
-//             super(props); 
-
-//             this.state = {
-//                 resName: " ", 
-//             }
-//             this.handleSubmit = this.handleSubmit.bind(this); 
-//     }
-   
-
-// //         };
-//     //  handleInput = resName => {
+//     // handleInput = resName => {
 //     //     return e => { this.setState({
-//     //         resName: e.target.value}); 
-//     //      }
+//     //         resName: e.target.value
+//     //     })}
 
-//     //      this.props.
-//     //         this.props.receiveRestaurant(e.target.value); 
-//     //     }
-
-    
-//     // // handleSubmit
-
-    
-//     // render(){
         
 
-//     //     return(
-//     //         <div className='search-bar'>
-//     //             <input className='search-form' type="search" placeholder= 'Search Restaurant Here'/>
-//     //             <button className='search-button'>Let's go</button>
-//     //         </div>
-//     //     )
 //     // }
 
+
 // }
 
-// export default SearchForm; 
+class SearchForm extends React.Component{
+
+    constructor(props){
+            super(props); 
+
+            this.state = {
+                resName: " ", 
+            }
+            // this.handleSubmit = this.handleSubmit.bind(this); 
+    }
+   
+
+//         };
+    //  handleInput = resName => {
+    //     return e => { this.setState({
+    //         resName: e.target.value}); 
+    //      }
+
+    //      this.props.
+    //         this.props.receiveRestaurant(e.target.value); 
+    //     }
+
+    
+    // // handleSubmit
+
+    
+    render(){
+        
+
+        return(
+            <div className='search-bar'>
+                <input className='search-form' type="search" placeholder= 'Search Restaurant Here'/>
+                <button className='search-button'>Let's go</button>
+            </div>
+        )
+    }
+
+}
+
+export default SearchForm; 
