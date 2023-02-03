1c0378e4e3a3ff99be6390e95171a26e11b3eb2c
lavparker <lavinia.parker@outlook.com>
2023-02-03 13:01:39 -0500

    reservation updates

--- frontend/components/reservations/reservation_update_container.jsx
+++ frontend/components/reservations/reservation_update_container.jsx
@@ -1,23 +1,30 @@
 import { connect } from 'react-redux';
 import { getReservation, updateReservation } from '../../actions/reservation_actions';
 import ReservationForm from "./reservation_form";
+import ReservationUpdateForm from "./reservation_update_form";
+import { openModal, closeModal } from '../../actions/modal_actions';
 
 
 const mapStateToProps = (state, ownProps) => {
-    return{
-        // currentUser: state.session.currentUser,
-        reservation: state.entities.reservations[ownProps.reservationId],
-        // restaurant: state.entities.restaurants[ownProps.match.params.restaurantId]
-        formHeader: "Update your reservation"
-        
-    }
+    return {
+      // currentUser: state.session.currentUser,
+      //   reservation: state.entities.reservations[ownProps.reservationId],
+      reservation: state.reservations[ownProps.reservationId],
+
+      // restaurant: state.entities.restaurants[ownProps.match.params.restaurantId]
+      formHeader: "Update your reservation",
+    };
 }
 
 const mapDispatchToProps = dispatch => ({
     // getReservation: reservationId => dispatch(getReservation(reservationId)),
     // updateReservation: reservation => dispatch(updateReservation(reservation)),
-    handleSubmit: (reservation) => dispatch(updateReservation(reservation))// console.log('ive been updated')
+    handleSubmit: (reservation) => dispatch(updateReservation(reservation)),// console.log('ive been updated')
     //dispatch appropriate action to update the reservation 
+    closeModal: () => dispatch(closeModal())
 })
 
-export default connect(mapStateToProps, mapDispatchToProps)(ReservationForm); 
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ReservationUpdateForm); 
