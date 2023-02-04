9f87624f6961b88277e808897a5601acc582c3c4
lavparker <lavinia.parker@outlook.com>
2023-02-03 20:12:19 -0500

    review controller  complete

--- app/controllers/api/favorites_controller.rb
+++ app/controllers/api/favorites_controller.rb
@@ -1,2 +1,3 @@
 class Api::FavoritesController < ApplicationController
+    
 end
--- app/controllers/api/reviews_controller.rb
+++ app/controllers/api/reviews_controller.rb
@@ -1,2 +1,52 @@
 class Api::ReviewsController < ApplicationController
+    skip_before_action :verify_authenticity_token
+    before_action :require_logged_in!, only: [:index, :create, :show, :update, :destroy]
+
+    def index
+        @review = Review.all
+        render :index
+    end
+
+    def show
+        @review = Review.find(params[:id])
+        render :show
+    end
+
+    def create
+        @review = Review.new(review_params)
+        if @review.save
+            render :show
+        else
+            render json: @review.errors.full_messages, status: 422
+        end
+    end
+
+    def update
+        @review = Review.find(params[:id])
+
+        if @review && @review.update(review_params)
+            render :show
+        else
+            render json: @review.errors.full_messages, status: 422
+        end
+    end
+
+    def destroy
+        @review = Review.find_by(id: params[:id])
+
+        if @review
+            @review.destroy
+            render :show
+        else
+            render json: ['Error: This deletion request could not be completed'], status: 422
+        end
+
+        private
+        def review_params
+            params.require(:review).permit(:restaurant_id, :user_id, :overall_rating, :food_rating, :ambiance_rating, :value_rating, :noise_level, :body)
+        end
 end
+
+
+
+
