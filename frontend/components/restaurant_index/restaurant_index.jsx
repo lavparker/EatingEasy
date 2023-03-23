d0e0c0e441dec722169f26fc9ec7dc267986e8dc
lavparker <lavinia.parker@outlook.com>
2023-03-23 13:51:01 -0400

    updated code for restaurant index

--- frontend/components/restaurant_index/restaurant_index.jsx
+++ frontend/components/restaurant_index/restaurant_index.jsx
@@ -1,40 +1,38 @@
 import React from "react";
 import RestaurantIndexItem from "./restaurant_index_item";
 
+class RestaurantIndex extends React.Component {
+  // constructor(props){
+  //     super(props);
 
-class RestaurantIndex extends React.Component{
-    // constructor(props){
-    //     super(props);
+  //     // this.state= {}
+  // }
 
-    //     // this.state= {}
-    // }
+  componentDidMount() {
+    this.props.getRestaurants();
+  }
 
-    componentDidMount(){
-        this.props.getRestaurants(); 
-    }
+  render() {
+    // const { restaurants } = this.props;
+    // debugger for reference :)
 
-    render(){
-        // const { restaurants } = this.props;
-        // debugger for reference :) 
-    
-        return(
-            <div className="restaurants-index">
-                <h2 >Local Restaurants</h2>
-                
-                <ul className="local-restaurants">
-                    {
-                        this.props.restaurants.map(restaurant => 
-                             <RestaurantIndexItem
-                                restaurant={restaurant}
-                                key={restaurant.id}
-                            />
-                        )
-                    }
-                </ul>
-            </div>
-        )
-    }
+    return (
+      <div className="restaurants-index">
+        <h2>Local Restaurants</h2>
+
+        <ul className="local-restaurants">
+           {
+                this.props.restaurants.map( restaurant => 
+                    <RestaurantIndexItem
+                        restaurant={restaurant} 
+                        key={restaurant.id} 
+                        />
+                )
+            }
+        </ul>
+      </div>
+    );
+  }
 }
 
-export default RestaurantIndex; 
-
+export default RestaurantIndex;
