bec7af96ec38134804aff5e6b2a2d57e96b1c065
lavparker <lavinia.parker@outlook.com>
2023-04-19 18:36:15 -0400

    got reviews to render

--- frontend/components/reviews/review_index_item.jsx
+++ frontend/components/reviews/review_index_item.jsx
@@ -11,13 +11,22 @@ class ReviewIndexItem extends React.Component{
     }
 
     render(){
-        const { review } = this.props; 
-
+        const { reviews } = this.props; 
+        const { reservation } = this.props;
         return (
           <div className="review-item-container">
-            {/* <div> this is working {review.overall}</div> */}
-            
-            <div> this is working</div>
+            <ul className="review-details">
+              <li className="current-user">Name: </li>
+              {/* <li className="review-date">Dined on: {reservation.date}</li> */}
+              <li className="review-overall">Overall: {reviews.over_all}</li>
+              <li className="review-food">Food: {reviews.food_rating}</li>
+              <li className="review-ambiance">
+                Ambiance: {reviews.ambiance_rating}
+              </li>
+              <li className="review-noise">Noise: {reviews.noise_level}</li>
+              <li className="review-value">Value: {reviews.value_rating}</li>
+              <li className="review-body">{reviews.body}</li>
+            </ul>
 
             <FaRegTrashAlt />
           </div>
