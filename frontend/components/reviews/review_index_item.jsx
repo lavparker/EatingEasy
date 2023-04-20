bebd526545fce201b6a5c46fe7e16146a63574be
lavparker <lavinia.parker@outlook.com>
2023-04-20 00:09:41 -0400

    added update button

--- frontend/components/reviews/review_index_item.jsx
+++ frontend/components/reviews/review_index_item.jsx
@@ -17,27 +17,29 @@ class ReviewIndexItem extends React.Component{
        
         return (
           <div className="review-item-container">
-            <ul className="review-details">
-              <li className="current-user">
-                Restaurant Id (for my reference): {reviews.restaurant_id}
-              </li>
-              {/* <li className="review-date">Dined on: {reservation.date}</li> */}
-              <li className="review-overall">Overall: {reviews.over_all}</li>
-              <li className="review-food">Food: {reviews.food_rating}</li>
-              <li className="review-ambiance">
-                Ambiance: {reviews.ambiance_rating}
-              </li>
-              <li className="review-noise">Noise: {reviews.noise_level}</li>
-              <li className="review-value">Value: {reviews.value_rating}</li>
-              <li className="review-body">{reviews.body}</li>
-            </ul>
-            <button
-              className="delete-res"
-              onClick={() => this.props.deleteReview(reviews.id)}
-            >
-              <FaRegTrashAlt className="trash-can-btn" /> Delete Review{" "}
-            </button>
-          
+            <div className='review-index-item'>
+              <ul className="review-details">
+                <li className="current-user">
+                  Restaurant Id (for my reference): {reviews.restaurant_id}
+                </li>
+                {/* <li className="review-date">Dined on: {reservation.date}</li> */}
+                <li className="review-overall">Overall: {reviews.over_all}</li>
+                <li className="review-food">Food: {reviews.food_rating}</li>
+                <li className="review-ambiance">
+                  Ambiance: {reviews.ambiance_rating}
+                </li>
+                <li className="review-noise">Noise: {reviews.noise_level}</li>
+                <li className="review-value">Value: {reviews.value_rating}</li>
+                <li className="review-body">{reviews.body}</li>
+              </ul>
+              <button
+                className="delete-res"
+                onClick={() => this.props.deleteReview(reviews.id)}
+              >
+                <FaRegTrashAlt className="trash-can-btn" /> Delete Review{" "}
+              </button>
+              <button className='modify-res'><FaPencilAlt className='pencil-btn'/>Modify Review </button>
+            </div>
           </div>
         );
     }
