39431440ec132f7f3715251678c69f39d5673861
lavparker <lavinia.parker@outlook.com>
2023-06-06 23:33:02 -0400

    fixed css for reviews on restaurant show page

--- frontend/components/reviews/review_index_item.jsx
+++ frontend/components/reviews/review_index_item.jsx
@@ -8,18 +8,11 @@ class ReviewIndexItem extends React.Component{
     constructor(props){
         // debugger
         // console.log("REVIEW INDEX ITEM PROPS", props);
-        super(props);
-
-        // this.state = {
-        //   editingReview: false,
-        // };
+        super(props); 
     }
 
-
-    
-
     render(){
-        const { reviews } = this.props; 
+         const { reviews } = this.props; 
         const { reservation } = this.props;
         const { currentUser } = this.props;
         // console.log("THESE ARE THE REVIEW PROPS", reviews); 
@@ -29,35 +22,40 @@ class ReviewIndexItem extends React.Component{
         // let currentUserId = session.id;
       
         // console.log("the current user id is", currentUser.id)
-       
-        return (
-          <div className="review-item-container">
-            {/* <div className="review-index-item"> */}
-              <ul className="review-details">
-                <li className="review-firstname">
-                  <section className="column-1">
-                    {currentUser.first_name[0]}
-                  </section>
+  
+      return (
+        <div className="review-item-container">
+          <div className="review-details-left">
+            <li className="review-firstname">
+              <section className="column-1">
+                {currentUser.first_name[0]}
+              </section>
+            </li>
+          </div>
+          <div className="review-details-right">
+            <div className="column-2">
+              <div className="review-details-right-top">
+                <li className="review-overall">
+                  Overall &nbsp; {reviews.overall_rating} &nbsp; |{" "}
                 </li>
-                {/* <li className="current-user">Restaurant Id (for my reference): {reviews.restaurant_id}</li> */}
-                {/* <li className="review-userId">User Id: {reviews.user_id}</li> */}
-                {/* <li className="review-id">Review Id: {reviews.id}</li> */}
-                {/* <li className="review-date">Dined on: {reservation.date}</li> */}
-                <section className='column-2'>
-                  <li className="review-overall">
-                    Overall: {reviews.overall_rating}
-                  </li>
-                  <li className="review-food">Food: {reviews.food_rating}</li>
-                  <li className="review-ambiance">
-                    Ambiance: {reviews.ambiance_rating}
-                  </li>
-                  <li className="review-noise">Noise: {reviews.noise_level}</li>
-                  <li className="review-value">
-                    Value: {reviews.value_rating}
-                  </li>
-                  <li className="review-body">{reviews.body}</li>
-                </section>
-              </ul>
+                <li className="review-food">
+                  Food &nbsp; {reviews.food_rating} &nbsp; |{" "}
+                </li>
+                <li className="review-ambiance">
+                  Ambiance &nbsp; {reviews.ambiance_rating} &nbsp; |{" "}
+                </li>
+                <li className="review-noise">
+                  Noise &nbsp; {reviews.noise_level} &nbsp; |{" "}
+                </li>
+                <li className="review-value">
+                  Value &nbsp; {reviews.value_rating}{" "}
+                </li>
+              </div>
+              <div className="review-details-right-mid">
+                <li className="review-body">{reviews.body}</li>
+              </div>
+            </div>
+            <div className="review-details-right-bottom">
               {reviews && reviews.user_id === currentUser.id && (
                 <div className="review-res-buttons">
                   <button
@@ -76,10 +74,17 @@ class ReviewIndexItem extends React.Component{
                   </Link>
                 </div>
               )}
-            {/* </div> */}
+            </div>
           </div>
-        );
+        </div>
+      );
     }
-}
+     
+          
+              
 
-export default ReviewIndexItem; 
+  
+}
+  
+
+export default ReviewIndexItem;
